eb13389e21d3c51a312bdc0728f78352050da6de
theGeekyLad <rahul.pillai03@gmail.com>
2020-11-27 11:28:02 +0530

    Migrated everything to 1 .sh

--- crestfallen-git.sh
+++ crestfallen-git.sh
@@ -1,1 +1,11 @@
-python3 /home/thegeekylad/Projects/py-crestfallen-git/crestfallen-git.py
+# DO THIS INSIDE .bash_profile
+# echo $XAUTHORITY > cresfallen-git-xauthority
+
+shouldSuspend=$(XAUTHORITY=$(cat /home/thegeekylad/.config-cresfallen-git) DISPLAY=":0" python3 /home/thegeekylad/Projects/py-crestfallen-git/crestfallen-git.py)
+if [ -z "$shouldSuspend" ];
+then
+        echo 'Suspending...'
+        systemctl suspend
+else
+        echo 'Skipping suspend...'
+fi
